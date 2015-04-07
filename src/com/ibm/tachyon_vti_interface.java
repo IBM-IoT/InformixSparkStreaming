2712b9105571d0a232eca39f57ce505c5a923692
Kenneth Myers <myerske@us.ibm.com>
2015-04-07 14:30:53 -0500

    Very trivial implemenation of am_create. Created a file on the tachyon system which will serve as the table.

--- src/com/ibm/tachyon_vti_interface.java
+++ src/com/ibm/tachyon_vti_interface.java
@@ -1,23 +1,32 @@
 package com.ibm;
 import org.mapdb.*;
+import tachyon.TachyonURI;
+import tachyon.Constants;
+import tachyon.TachyonURI;
+import tachyon.Version;
+import tachyon.client.OutStream;
+import tachyon.client.TachyonByteBuffer;
+import tachyon.client.TachyonFile;
+import tachyon.client.TachyonFS;
+import tachyon.client.WriteType;
+import tachyon.util.CommonUtils;
+import tachyon.client.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 import java.util.concurrent.ConcurrentNavigableMap;
 
 
 public class tachyon_vti_interface {
+    private static final TachyonURI mMasterLocation = new TachyonURI("tachyon://127.0.0.1:19998/");
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Tachyon VTI Prototype");
+        String test = "test";
+        am_create(test);
 
-
-
-        DB db = DBMaker.newMemoryDB().make();
-
-        ConcurrentNavigableMap treeMap = db.getTreeMap("map");
-        treeMap.put("something","here");
-
-        db.commit();
-        db.close();
 
     }
 
@@ -29,8 +38,18 @@ public class tachyon_vti_interface {
         return 0;
     }
 
-    public int am_create (String MI_AM_TABLE_DESC){
+    public static int am_create (String MI_AM_TABLE_DESC){
+        try {
+            TachyonFS tachyonClient = TachyonFS.get(mMasterLocation);
+            createFile(tachyonClient);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return 0;
+    }
+
+    private static void createFile(TachyonFS tachyonClient) throws IOException {
+        int fileId = tachyonClient.createFile("/table");
     }
 
 }
