cdcad90c9ac724a9cae74408f27f8e9b31490c95
Kenneth Myers <myerske@us.ibm.com>
2015-04-09 13:26:47 -0500

    Added function creation and creation of primary access methods.

--- register_UDRs.sql
+++ register_UDRs.sql
@@ -10,7 +10,63 @@ CREATE PROCEDURE connect_to_tachyon()
    EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.connect_to_tachyon()'
    LANGUAGE java;
 
+CREATE FUNCTION disconnectFromTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.disconnectFromTachyon()'
+LANGUAGE java;
+
 CREATE FUNCTION connect_to_tachyon(pointer)
 RETURNING integer
 EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.connect_to_tachyon()'
 LANGUAGE java;
+
+
+CREATE FUNCTION createTableInTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.createTableInTachyon()'
+LANGUAGE java;
+
+CREATE FUNCTION deleteTableInTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.deleteTableInTachyon()'
+LANGUAGE java;
+
+CREATE FUNCTION beginScanTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.beginScanTachyon()'
+LANGUAGE java;
+
+CREATE FUNCTION getNextTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.getNextTachyon()'
+LANGUAGE java;
+
+CREATE FUNCTION endScanTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.endScanTachyon()'
+LANGUAGE java;
+
+CREATE FUNCTION insertTachyon(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.insertTachyon()'
+LANGUAGE java;
+
+CREATE FUNCTION getByte(pointer)
+RETURNING integer
+EXTERNAL NAME 'informix_tachyon:TachyonVtiInterface.getByte()'
+LANGUAGE java;
+
+CREATE PRIMARY ACCESS_METHOD my_virtual
+(AM_OPEN = connect_to_tachyon,
+AM_CLOSE = disconnectFromTachyon,
+AM_CREATE = createTableInTachyon,
+AM_DROP = deleteTableInTachyon,
+AM_BEGINSCAN =beginScanTachyon,
+AM_GETNEXT = getNextTachyon,
+AM_ENDSCAN = endScanTachyon,
+AM_INSERT = insertTachyon,
+AM_GETBYID = getByte,
+AM_READWRITE,
+AM_ROWIDS,
+AM_SPTYPE = 'X',
+AM_CLUSTER)
