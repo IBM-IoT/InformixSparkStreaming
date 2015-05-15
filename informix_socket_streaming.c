c169273f647e0efceb66011bc623b4b93998b81b
Kenneth Myers <myerske@us.ibm.com>
2015-05-15 11:33:04 -0500

    Reorganized some code. Changed where sockets are created/destroyed so that sockets can be resued.

--- informix_socket_streaming.c
+++ informix_socket_streaming.c
@@ -15,25 +15,84 @@ typedef mi_unsigned_integer mi_uint;
 typedef mi_double_precision mi_double;
 
 int socket_file_descriptor;
+int newsocket_file_descriptor;
+char* informix_directory;
+FILE *log_file;
+char buffer[256];
+
+MI_DECL mi_integer
+tachyonOpen (mi_pointer *tableDesc)
+{
+    // Create the log file we will use throughout the program.
+    log_file = fopen("/opt/informix/tmp/open.log", "w");
+    if (log_file == NULL)
+    {
+        printf("Error creating log file.\n");
+    }
+
+   // Allow up to 512 rows to be inserted/updated with a single SQL statement.
+   mi_integer rows = mi_tab_setniorows(tableDesc, 512);
+   fprintf(log_file, "mi_tab_setniorows return value %d\n", rows);
+   fclose(log_file);
+   return 0;
+}
+
+MI_DECL mi_integer
+tachyonClose (mi_pointer *buf)
+{
+    return 0;
+}
 
 MI_DECL mi_integer
 tachyonCreate (mi_pointer *buf)
 {
-    FILE *f = fopen("socket_streaming.log", "w");
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-    }
-
+    log_file = fopen("/opt/informix/tmp/create.log", "w");
     // Open the socket when we create the index.
     socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_file_descriptor < 0){
-        fprintf(f, "Error opening socket.\n");
+        fprintf(log_file, "Error opening socket.\n");
     }
     else{
-    fprintf(f, "Successfully created socket with file descriptor: %d\n", socket_file_descriptor);
+        fprintf(log_file, "Successfully created socket with file descriptor: %d\n", socket_file_descriptor);
     }
 
+    const int       optVal = 1;
+    const socklen_t optLen = sizeof(optVal);
+
+    int rtn = setsockopt(socket_file_descriptor, SOL_SOCKET, SO_REUSEADDR, (void*) &optVal, optLen);
+
+    int portno;
+    socklen_t clilen;
+
+    struct sockaddr_in serv_addr, cli_addr;
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(13338);
+    fprintf(log_file, "addr: %l\n", serv_addr.sin_addr.s_addr);
+    fprintf(log_file, "port: %d\n", serv_addr.sin_port);
+    fprintf(log_file, "socket_file_descriptor %d \n", socket_file_descriptor);
+    if (bind(socket_file_descriptor, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+        fprintf(log_file, "Bind failed \n");
+        fprintf(log_file, "errno: %d \n", errno);
+    }
+
+    listen(socket_file_descriptor,5);
+    clilen = sizeof(cli_addr);
+    newsocket_file_descriptor = accept(socket_file_descriptor,
+                                       (struct sockaddr *) &cli_addr,
+                                       &clilen);
+
+    rtn = setsockopt(newsocket_file_descriptor, SOL_SOCKET, SO_REUSEADDR, (void*) &optVal, optLen);
+
+    if (newsocket_file_descriptor < 0) {
+        fprintf(log_file, "accept failed\n");
+        fprintf(log_file, "errno: %d \n", errno);
+    }
+    fclose(log_file);
     return 0;
 }
 
@@ -42,105 +101,43 @@ tachyonDrop (mi_pointer *buf)
 {
     close(newsocket_file_descriptor);
     close(socket_file_descriptor);
-}
-
-MI_DECL mi_integer
-tachyonOpen (mi_pointer *buf)
-{
-    FILE *f = fopen("socket_streaming.log", "w");
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-    }
-
-    mi_integer rows = a(buf, 512);
-    fprintf(f, "mi_tab_setniorows return value %d\n", rows);
-
-    fclose(f);
-     return 0;
-}
-
-MI_DECL mi_integer
-tachyonClose (mi_pointer *buf)
-{
-  return 0;
-}
-
-MI_DECL mi_integer
-tachyonGetByid (mi_pointer *buf)
-{
-  return 0;
-}
-
-MI_DECL mi_integer
-tachyonGetNext (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2)
-{
-  return 0;
+    return 0;
 }
 
 MI_DECL mi_integer
 tachyonInsert (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2)
 {
-FILE *f = fopen("socket_streaming.log", "w");
-  if (f == NULL)
-  {
-    printf("Error opening file!\n");
-  }
-
-    int socket_file_descriptor, newsocket_file_descriptor, portno;
-    socklen_t clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
     int n;
-    socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_file_descriptor < 0) fprintf(f, "Error opening socket \n");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(12517);
-    fprintf(f, "addr: %l\n", serv_addr.sin_addr.s_addr);
-    fprintf(f, "port: %d\n", serv_addr.sin_port);
-    fprintf(f, "socket_file_descriptor %d \n", socket_file_descriptor);
-    if (bind(socket_file_descriptor, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
-        fprintf(f, "Bind failed \n");
-        fprintf(f, "errno: %d \n", errno);
+    log_file = fopen("/opt/informix/tmp/insert.log", "w");
+    if (log_file == NULL)
+    {
+        printf("Error opening file!\n");
     }
 
 
-    listen(socket_file_descriptor,5);
-    clilen = sizeof(cli_addr);
-    newsocket_file_descriptor = accept(socket_file_descriptor,
-                       (struct sockaddr *) &cli_addr,
-                       &clilen);
 
 
-    if (newsocket_file_descriptor < 0) {
-        fprintf(f, "accept failed\n");
-        fprintf(f, "errno: %d \n", errno);
-    }
-
-    bzero(buffer,256);
+    bzero(buffer, 256);
 
     MI_ROW *row = NULL;
     mi_integer rowid = 0;
     mi_integer fragid = 0;
 
     mi_integer nrows = mi_tab_niorows(buf0);
-    fprintf(f, "Number of rows: %d\n", nrows);
+    fprintf(log_file, "Number of rows: %d\n", nrows);
 
     mi_integer x = mi_tab_nextrow(buf0, &row, &rowid, &fragid);
 
     mi_integer numcols;
     numcols = mi_column_count(row);
-    fprintf(f, "Number of columns: %d\n", numcols);
+    fprintf(log_file, "Number of columns: %d\n", numcols);
 
     mi_string *colname;
     int i = 0;
     while( i < numcols )
     {
         colname = mi_column_name(row, i);
-        fprintf(f, " %s\t", colname);
+        fprintf(log_file, " %s\t", colname);
         i++;
     }
 
@@ -157,20 +154,31 @@ FILE *f = fopen("socket_streaming.log", "w");
         n = write(newsocket_file_descriptor, buffer, length + 1);
 
         if (n < 0){
-            fprintf(f, "Write failed \n");
-            fprintf(f, "errno: %d \n", errno);
+            fprintf(log_file, "Write failed \n");
+            fprintf(log_file, "errno: %d \n", errno);
         }
-        fprintf(f, "%s\t", buffer);
+        fprintf(log_file, "%s\t", buffer);
     }
 
-    close(newsocket_file_descriptor);
-    close(socket_file_descriptor);
+    //close(newsocket_file_descriptor);
+    //close(socket_file_descriptor);
 
-    fclose(f);
+    fclose(log_file);
 
-    return 0;
+return 0;
 }
 
+MI_DECL mi_integer
+tachyonGetByid (mi_pointer *buf)
+{
+  return 0;
+}
+
+MI_DECL mi_integer
+tachyonGetNext (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2)
+{
+  return 0;
+}
 
 tachyonDelete (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2)
 {
@@ -179,7 +187,7 @@ tachyonDelete (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2)
 
 tachyonUpdate (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2, mi_pointer *buf3, mi_pointer *buf4)
 {
-    FILE *f = fopen("/opt/informix/update.txt", "w");
+/*    FILE *f = fopen("/opt/informix/update.txt", "w");
     if (f == NULL)
     {
         printf("Error opening file!\n");
@@ -264,7 +272,7 @@ tachyonUpdate (mi_pointer *buf0, mi_pointer *buf1, mi_pointer *buf2, mi_pointer 
         n = write(newsocket_file_descriptor, buffer, length + 1);
         fprintf(f, "%d\t", full_int);
     }
-    fclose(f);
+    fclose(f);*/
     return 0;
 }
 
