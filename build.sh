0e1f62b53e01bd9f8a9e0a5e5812a36736503005
Cristian Traistaru <traistar@us.ibm.com>
2016-06-15 16:49:51 -0500

    Remove old unnecessary files

--- build.sh
+++
@@ -1,3 +0,0 @@
-gcc -w -fPIC -DMI_SERVBUILD -I$INFORMIXDIR/incl/public  -I$INFORMIXDIR/incl -L$INFORMIXDIR/esql/lib -c informix_socket_streaming.c
-
-gcc informix_socket_streaming.o -shared -o informix_socket_streaming.so
